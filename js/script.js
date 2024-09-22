b72c63584db232733c561b63090347ab1f48b930
sochin-bhattacharjee <sochin.cs@gmail.com>
2024-09-22 23:10:20 +0600

    added switching donation & history Btn

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById('donation').addEventListener('click',function(){
+    document.getElementById('history').classList.remove('bg-lime-300')
+    document.getElementById('donation').classList.add('bg-lime-300')
+    document.getElementById('donationContent').classList.remove('hidden')
+    document.getElementById('historyContent').classList.add('hidden')
+})
+document.getElementById('history').addEventListener('click',function(){
+    document.getElementById('history').classList.add('bg-lime-300')
+    document.getElementById('donation').classList.remove('bg-lime-300')
+    document.getElementById('historyContent').classList.remove('hidden')
+    document.getElementById('donationContent').classList.add('hidden')
+})
